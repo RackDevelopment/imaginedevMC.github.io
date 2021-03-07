aa00c60d1410158677002ec6fc059a1157bdc6d2
Leonardo Bishop <13875753+LMBishop@users.noreply.github.com>
2021-03-07 02:47:13 +0000

    use jquery

--- contributors.js
+++ contributors.js
@@ -1,18 +1,18 @@
 const REQUEST_URL = 'https://api.github.com/repos/imaginedevMC/imaginedevMC.github.io/contributors'
 
-$(document).ready(function() {
+$(document).ready(() => {
     $.ajax({
         type: "GET",
         url: REQUEST_URL,
 
-        success: function (data) {
+        success: data => {
             var contributors = []
 
             for (var obj of data) {
                 contributors.push(obj.login)
             }
             
-            document.getElementById('contributors').innerHTML = contributors.join()
+            $('#contributors').html(contributors.join());
         }
     })
 });
